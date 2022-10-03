0cf894882b31a5fd893030d9731cb8598fc1515a
Xandyssz <xandyofcff@gmail.com>
2022-10-03 11:52:41 -0300

    Atualização Banco de Dados

--- BD/ifsp_lacif.sql
+++ BD/ifsp_lacif.sql
@@ -1,31 +1,48 @@
 create table consultas
 (
-    idconsulta    int auto_increment
-        primary key,
-    nome          varchar(50)                    not null,
-    cpf           char(11)                       not null,
-    celular       char(11)                       not null,
+    id            int auto_increment             primary key,
+    title         varchar(90)                    not null,
+    description   text                           not null,
+    start         date                           not null,
+    end           date                           not null,
     convenio      varchar(50)                    not null,
-    data_cons     date                           not null,
-    horario_cons  time                           not null,
+    celular       varchar(40)                    not null,
+    cpf           varchar(40)                    not null,
     tiposanguineo varchar(4)                     not null,
-    sexo          varchar(15)                    not null,
     tipo          varchar(50)                    not null,
+    sexo          varchar(15)                    not null,
     status        varchar(50) default 'Pendente' not null
+);
+
+create table noticias
+(
+    idNoticia   int auto_increment      primary key,
+    titulo      varchar(50)             not null,
+    descricao   varchar(500)            not null,
+    dataNoticia date                    not null,
+    imagem      text                    not null
+);
+
+create table pagamento
+(
+    idpagamento      int auto_increment     primary key,
+    status           int                    not null,
+    metodo_pagamento varchar(45)            not null,
+    valor            double                 not null,
+    valor_pago       double                 not null,
+    data_pagamento   date                   not null
 );
 
 create table usuarios
 (
-    idusuario   int auto_increment
-        primary key,
+    idusuario   int auto_increment             primary key,
     nome        varchar(50)                    not null,
-    cpf         char(11)                       not null,
+    cpf         varchar(40)                    not null,
     email       varchar(200)                   not null,
-    senha       varchar(35)                    not null,
-    celular     char(11)                       not null,
+    senha       varchar(250)                   not null,
+    celular     varchar(40)                    not null,
     endereco    varchar(200)                   not null,
     datanasc    date                           not null,
-    nivelAcesso varchar(35) default 'Paciente' not null
+    tipo_acesso varchar(45) default 'Paciente' not null
 );
 
-a
