253930839eec49440a398c1354510695968e2721
Xandyssz <xandyofcff@gmail.com>
2022-10-13 14:37:30 -0300

    Atualização MCT

--- IncludeExames.php
+++ IncludeExames.php
@@ -20,8 +20,13 @@
                     <p class="card__exit"><?php echo "<font color='black'>Data do Exame: " . $row['start'] . "</font>"; ?></p>
 
                     <h2 class="card__title"><?php echo "<font color='black'>Status do Exame: " . $row['status'] . "</font>"; ?></h2>
-                    <a href="lacif_noticias.php" class="navbar-btn">LEIA MAIS</a>
 
+                    <?php echo '<a href="', $row['resultado'], '">Download de '?>
+
+                    <a href="img/<?php echo $row['resultado'], '">Download de '?>
+
+
+                    <a href="lacif_noticias.php" class="navbar-btn">LEIA MAIS</a>
                 </div>
 
             </div>
